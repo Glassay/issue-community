772d00d634e7440c434f33c392824d254173c053
Glassay <31647920+Glassay@users.noreply.github.com>
2018-05-06 21:25:30 +0800

    评论区域布局

--- issueCommunity/src/components/Comment/Comment.js
+++ issueCommunity/src/components/Comment/Comment.js
@@ -6,13 +6,19 @@
 import React from 'react';
 import { Avatar, Divider } from 'antd';
 
+import styles from './Comment.less';
+
 class Comment extends React.Component {
   render() {
     return(
-      <div>
-        <Avatar size="large" icon="http://file5.u148.net/2015/07/images/14373784924538J3J6KEQ3.jpg" />
-        <em>林荫蛮</em>
-        <span>2018-05-06</span>
+      <div className={styles.layout}>
+        <div className={styles.head}>
+          <Avatar size="large" src="http://file5.u148.net/2015/07/images/14373784924538J3J6KEQ3.jpg" />
+          <div className={styles.name}>
+            <em>林荫蛮</em>
+            <span>2018-05-06</span>
+          </div>
+        </div>
         <article>真正的写作，是一件很纯粹的事，是一种单纯的喜爱、洁净的创作，无关酬劳，无关声誉，无关结果，所有的意义，都在字里行间，都在那个纯粹的过程里。</article>
         <Divider />
       </div>
--- issueCommunity/src/components/Comment/InputArea.js
+++ issueCommunity/src/components/Comment/InputArea.js
@@ -2,3 +2,25 @@
  * 2018-05-06 Glassay
  * 评论输入框
  */
+
+import React from 'react';
+import { Input, Button, Icon } from 'antd';
+
+import styles from './InputArea.less';
+
+const { TextArea } = Input;
+
+class InputArea extends React.Component {
+  render() {
+    return(
+      <div>
+        <TextArea placeholder="在此输入评论内容！" autosize={{ minRows: 2, maxRows: 6 }} />
+        <Input placeholder="上传图片" className={styles.upload} />
+        <Input placeholder="上传视频" className={styles.upload} />
+        <Button className={styles.button}>发布</Button>
+      </div>
+    );
+  }
+}
+
+export default InputArea;
