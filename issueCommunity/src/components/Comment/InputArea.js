a9425ce6852742c4dd1a361709b1cca900b4cedb
Glassay <31647920+Glassay@users.noreply.github.com>
2018-05-12 21:39:12 +0800

    评论发布按钮样式调整

--- issueCommunity/src/components/Comment/InputArea.js
+++ issueCommunity/src/components/Comment/InputArea.js
@@ -17,7 +17,7 @@ class InputArea extends React.Component {
         <TextArea placeholder="在此输入评论内容！" autosize={{ minRows: 2, maxRows: 6 }} />
         <Input placeholder="上传图片" className={styles.upload} />
         <Input placeholder="上传视频" className={styles.upload} />
-        <Button className={styles.button}>发布</Button>
+        <Button type="primary" className={styles.register}>发布</Button>
       </div>
     );
   }
