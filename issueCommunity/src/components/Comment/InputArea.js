fb3d8c591d2bb9ed65160411f4d342320c8143a2
Glassay <31647920+Glassay@users.noreply.github.com>
2018-05-13 12:25:16 +0800

    上传文件

--- issueCommunity/src/components/Comment/InputArea.js
+++ issueCommunity/src/components/Comment/InputArea.js
@@ -4,19 +4,37 @@
  */
 
 import React from 'react';
-import { Input, Button } from 'antd';
+import { Input, Button, Icon, Upload, message } from 'antd';
 
 import styles from './InputArea.less';
 
 const { TextArea } = Input;
+const props = {
+  name: 'file',
+  action: '//jsonplaceholder.typicode.com/posts/',
+  headers: {
+    authorization: 'authorization-text',
+  },
+  onChange(info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
 
 class InputArea extends React.Component {
   render() {
     return(
       <div>
         <TextArea placeholder="在此输入评论内容！" autosize={{ minRows: 2, maxRows: 6 }} />
-        <Input placeholder="上传图片" className={styles.upload} />
-        <Input placeholder="上传视频" className={styles.upload} />
+        <Upload {...props}>
+          <Icon style={{ fontSize: 30, marginTop: 10 }} type="picture" />
+        </Upload>
         <Button type="primary" className={styles.register}>发布</Button>
       </div>
     );
